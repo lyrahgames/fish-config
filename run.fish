ce68050b3ff31707f29a0a840861ea2c033ff1d0
lyrahgames <lyrahgames@mailbox.org>
2021-10-02 19:02:50 +0200

    Provide log messages and exit interactive shell

--- run.fish
+++ run.fish
@@ -5,20 +5,24 @@ set fish_config_dir $HOME/.config/fish
 # Create fish configuration folder if it not already exists.
 mkdir -p fish_config_dir
 
-# Install custom configuration.
+echo "Installing custom configuration"
 cp config.fish $fish_config_dir/
 
-# Install custom functions.
+echo "Installing custom autoloading functions"
 cp functions/*.fish $fish_config_dir/functions/
 
-# Install fisher package manager.
+echo "Installing fisher package manager"
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-# Fulfill requirements for additional plugins.
+echo "Fulfilling requirements for additional plugins"
 sudo pacman -Sy --needed --noconfirm bat fzf fd # Needed for fzf.fish
-# Copy fisher plugins file.
+echo "Copying fisher plugins file"
 cp fish_plugins $fish_config_dir/
-# Sync plugins by using fisher.
+echo "Syncing plugins by using fisher"
 fisher update
 
-# Set fish as default shell.
+echo "Setting fish as default shell"
 chsh -s /usr/bin/fish
+
+if status is-interactive
+  exit
+end
