df33472e1aaf690c70968a28b88f37d4a71119f0
lyrahgames <lyrahgames@mailbox.org>
2021-10-02 19:13:58 +0200

    Use sane log messages and 'which' requirement

--- run.fish
+++ run.fish
@@ -1,27 +1,30 @@
 #!/usr/bin/env fish
 
+# Fulfill system package requirements.
+sudo pacman -Sy --needed --noconfirm which bat fzf fd
+
+# Variables
 set fish_config_dir $HOME/.config/fish
 
 # Create fish configuration folder if it not already exists.
 mkdir -p fish_config_dir
 
-echo "Installing custom configuration"
-cp config.fish $fish_config_dir/
+echo "Installing custom configuration" \
+  && cp config.fish $fish_config_dir/
 
-echo "Installing custom autoloading functions"
-cp functions/*.fish $fish_config_dir/functions/
+echo "Installing custom autoloading functions" \
+  && cp functions/*.fish $fish_config_dir/functions/
 
-echo "Installing fisher package manager"
+# Handle additional plugins.
+# Install fisher package manager.
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-echo "Fulfilling requirements for additional plugins"
-sudo pacman -Sy --needed --noconfirm bat fzf fd # Needed for fzf.fish
-echo "Copying fisher plugins file"
-cp fish_plugins $fish_config_dir/
-echo "Syncing plugins by using fisher"
+echo "Copying fisher plugins file" \
+  && cp fish_plugins $fish_config_dir/
+# Sync plugins by using fisher.
 fisher update
 
-echo "Setting fish as default shell"
-chsh -s /usr/bin/fish
+# Set fish as default shell.
+chsh -s (which fish)
 
 if status is-interactive
   exit
