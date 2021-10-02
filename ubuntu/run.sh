5cb2f144e2957dde358521efdb2abf28c8fe5bed
lyrahgames <lyrahgames@mailbox.org>
2021-10-03 00:37:48 +0200

    Use latest fish version in Ubuntu

--- ubuntu/run.sh
+++ ubuntu/run.sh
@@ -1,7 +1,13 @@
 # Install required dependencies.
-sudo apt update \
-  && sudo apt install \
-    fish wget curl bat fzf fd-find
+sudo apt-get update \
+  && sudo apt-get install -y \
+    wget curl bat fzf fd-find
+
+# Install latest fish shell.
+sudo apt-add-repository ppa:fish-shell/release-3 \
+  && sudo apt-get update \
+  && sudo apt-get install -y \
+    fish
 
 # Run interactive fish script.
 fish --interactive scripts/run.fish
