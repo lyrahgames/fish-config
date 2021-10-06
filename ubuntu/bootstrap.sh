af1a47bf6f95ab82d1ca898d5a7f1b77d3b280f4
lyrahgames <lyrahgames@mailbox.org>
2021-10-06 16:09:54 +0200

    Remove confirmation query

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 # Install required dependencies.
-sudo apt-get update \
-  && sudo apt-get install -y \
-    software-properties-common
+sudo apt-get -yq update
+sudo apt-get -yq install \
+  software-properties-common
 
 # Install latest fish shell.
-sudo add-apt-repository ppa:fish-shell/release-3 \
-  && sudo apt-get update \
-  && sudo apt-get install -y \
-    fish
+sudo add-apt-repository -y \
+  ppa:fish-shell/release-3
+sudo apt-get -yq update
+sudo apt-get -yq install \
+  fish
 
 # Run interactive fish script.
 fish --login --interactive ubuntu/run.fish
